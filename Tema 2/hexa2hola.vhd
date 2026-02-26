573b9184882eccabfbff384277ed0a6082d72579
JESUS URIEL URIBE DIAZ <LedKutchi@gmail.com>
2026-02-26 22:47:06 +0000

    Actualizar el decodificador de hexadecimal a 7 segmentos en hexa2seg.vhd y agregar hexa2hola.vhd con funcionalidad similar.

--- Tema 2/hexa2hola.vhd
+++ Tema 2/hexa2hola.vhd
@@ -10,7 +10,7 @@ use ieee.numeric_std.all;
 ---------------------------------------------------------------------
 -- ENTIDAD
 ---------------------------------------------------------------------
-entity hexa2seg is
+entity hexa2hola is
   port (
     d3, d2, d1, d0: in std_logic;  -- entradas binarias
     a, b, c, d, e, f, g: out std_logic -- salidas al display
@@ -29,12 +29,12 @@ entity hexa2seg is
   attribute chip_pin of f: signal is "F3"; -- f del 3
   attribute chip_pin of g: signal is "D4"; -- g del 3
   
-end entity hexa2seg;
+end entity hexa2hola;
 
 ---------------------------------------------------------------------
 -- ARQUITECTURA
 ---------------------------------------------------------------------
-architecture una of hexa2seg is
+architecture una of hexa2hola is
   -- señales
 begin
   process (d3, d2, d1, d0) is
@@ -45,37 +45,37 @@ begin
     entrada := d3 & d2 & d1 & d0;
     case entrada is
       when "0000" =>
-        seg7 := "0000001"; -- cero
+        seg7 := "1001000"; -- H
       when "0001" =>
-        seg7 := "1001111"; -- uno
+        seg7 := "0000001"; -- O
       when "0010" =>
-        seg7 := "0010010"; -- dos
+        seg7 := "1110001"; -- L
       when "0011" =>
-        seg7 := "0000110"; -- tres
+        seg7 := "0001000"; -- A
       when "0100" =>
-        seg7 := "1001100"; -- cuatro
+        seg7 := "1111111"; -- (ESPACIO VACIO)
       when "0101" =>
-        seg7 := "0100100"; -- cinco
+        seg7 := "1110001"; -- L
       when "0110" =>
-        seg7 := "0100000"; -- seis
+        seg7 := "0000001"; -- O
       when "0111" =>
-        seg7 := "0001111"; -- siete
+        seg7 := "1110001"; -- L
       when "1000" =>
-        seg7 := "0000000"; -- ocho
+        seg7 := "0001000"; -- A
       when "1001" =>
-        seg7 := "0000100"; -- nueve
+        seg7 := "0111101"; -- (ESQUINA SUPERIOR IZQUIERDA) 
       when "1010" =>
-        seg7 := "0001000"; -- diez a
+        seg7 := "1110011"; -- (ESQUINA INFERIOR IZQUIERDA)
       when "1011" =>
-        seg7 := "1100000"; -- once b
+        seg7 := "1100111"; -- (ESQUINA INFERIOR DERECHA)
       when "1100" =>
-        seg7 := "0110001";  -- doce c
+        seg7 := "0011111";  -- (ESQUINA SUPERIOR DERECHA)
       when "1101" =>
-        seg7 := "1000010";  -- trece d
+        seg7 := "0111111";  -- (SEGMENTO ARRIBA)
       when "1110" =>
-        seg7 := "0110000";  -- catorce e
+        seg7 := "1110111";  -- (SEGMENTO ABAJO)
       when "1111" =>
-        seg7 := "0111000";  -- quince f
+        seg7 := "1111110";  -- (SEGMENTO CENTRAL)
       when others =>
         seg7 := "1111111";
     end case;
